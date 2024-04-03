be7328cff5d76efe48e6f68409abef60aa767e31
Phillip Davis <phdavis1027@gmail.com>
2024-04-03 17:38:54 -0400

    hacky script to create the kw enum

--- src/msg/key_val_pair.rs
+++ src/msg/key_val_pair.rs
@@ -5,20 +5,28 @@ use rods_prot_msg::error::errors::IrodsError;
 
 use crate::{
     bosd::{xml::BorrowingXMLSerializable, BorrowingSerializable},
+    common::kw::KeyWord,
     tag, tag_fmt,
 };
 
-#[cfg(not(test))]
-impl<'s> BorrowingSerializable<'s> for HashMap<&'s str, &'s str> {}
+#[cfg_attr(test, derive(Debug))]
+pub struct BorrowingStrStrMap<'s> {
+    vals: Vec<(KeyWord, &'s str)>,
+}
 
-#[cfg(test)]
-impl<'s> BorrowingSerializable<'s> for HashMap<&'s str, &'s str> {}
+impl<'s> BorrowingStrStrMap<'s> {
+    pub fn new() -> Self {
+        Self { vals: Vec::new() }
+    }
 
-impl<'s> BorrowingXMLSerializable<'s> for HashMap<&'s str, &'s str> {
-    fn borrowing_xml_serialize<'r>(
-        self,
-        mut sink: &'r mut Vec<u8>,
-    ) -> Result<usize, rods_prot_msg::error::errors::IrodsError>
+    pub fn insert(&mut self, key: KeyWord, value: &'s str) {
+        self.vals.push((key, value));
+    }
+}
+
+impl<'s> BorrowingSerializable<'s> for BorrowingStrStrMap<'s> {}
+impl<'s> BorrowingXMLSerializable<'s> for BorrowingStrStrMap<'s> {
+    fn borrowing_xml_serialize<'r>(self, sink: &'r mut Vec<u8>) -> Result<usize, IrodsError>
     where
         's: 'r,
     {
@@ -27,21 +35,14 @@ impl<'s> BorrowingXMLSerializable<'s> for HashMap<&'s str, &'s str> {
 
         writer.write_event(Event::Start(BytesStart::new("KeyValPair_PI")))?;
 
-        tag_fmt!(writer, "ssLen", "{}", self.len());
+        tag_fmt!(writer, "ssLen", "{}", self.vals.len());
 
-        self.keys().try_for_each(|k| {
-            tag!(writer, "keyWord", k);
-            Ok::<_, IrodsError>(())
-        })?;
-
-        self.values().try_for_each(|v| {
-            tag!(writer, "svalue", v);
-            Ok::<_, IrodsError>(())
-        })?;
+        for (key, _) in self.vals.iter() {
+            tag!(writer, "keyWord", key.into());
+        }
 
         writer.write_event(Event::End(BytesEnd::new("KeyValPair_PI")))?;
 
         Ok(cursor.position() as usize)
     }
 }
-
