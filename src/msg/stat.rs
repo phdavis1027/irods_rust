253825cdb46ebce729d00516c4ff722cf017cdc6
Phillip Davis <phdavis1027@gmail.com>
2024-04-12 13:18:38 -0400

    got stat msg types

--- src/msg/stat.rs
+++ src/msg/stat.rs
@@ -1,4 +1,5 @@
 use quick_xml::{events::Event, Reader};
+use rods_prot_msg::error::errors::IrodsError;
 
 use crate::{
     bosd::{xml::XMLDeserializable, Deserializable},
@@ -16,32 +17,6 @@ pub struct RodsObjStat {
     pub owner_zone: String,
     pub create_time: u64,
     pub modify_time: u64,
-}
-
-impl RodsObjStat {
-    pub fn new(
-        size: usize,
-        object_type: ObjectType,
-        mode: u32,
-        id: u32,
-        checksum: Option<u32>,
-        owner_name: String,
-        owner_zone: String,
-        create_time: u64,
-        modify_time: u64,
-    ) -> Self {
-        Self {
-            size,
-            object_type,
-            mode,
-            id,
-            checksum,
-            owner_name,
-            owner_zone,
-            create_time,
-            modify_time,
-        }
-    }
 }
 
 impl Deserializable for RodsObjStat {}
@@ -73,7 +48,6 @@ impl XMLDeserializable for RodsObjStat {
             ModifyTimeInner,
         }
 
-
         let mut size: Option<usize> = None;
         let mut object_type: Option<ObjectType> = None;
         let mut mode: Option<u32> = None;
@@ -91,20 +65,102 @@ impl XMLDeserializable for RodsObjStat {
         loop {
             state = match (state, reader.read_event()?) {
                 (State::Tag, Event::Start(e)) if e.name().as_ref() == b"RodsObjStat_PI" => {
-                    State::Size,
+                    State::Size
                 }
-                (State::Tag, Event::Start(e)) if e.name().as_ref() == b"objSize" => State::SizeInner,
+                (State::Tag, Event::Start(e)) if e.name().as_ref() == b"objSize" => {
+                    State::SizeInner
+                }
                 (State::SizeInner, Event::Text(e)) => {
                     size = Some(e.unescape()?.parse()?);
-                    State::ObjectType 
+                    State::ObjectType
                 }
-                (State::ObjectType, Event::Start(e)) if e.name().as_ref() == b"objType" => State::ObjectTypeInner,
+                (State::ObjectType, Event::Start(e)) if e.name().as_ref() == b"objType" => {
+                    State::ObjectTypeInner
+                }
                 (State::ObjectTypeInner, Event::Text(e)) => {
-                    object_type = Some(e.unescape()?.parse()?);
+                    object_type = Some(match e.unescape()?.parse::<u32>()? {
+                        0 => ObjectType::UnknownObj,
+                        1 => ObjectType::DataObj,
+                        2 => ObjectType::Coll,
+                        3 => ObjectType::UnknownFile,
+                        4 => ObjectType::LocalFile,
+                        5 => ObjectType::LocalDir,
+                        6 => ObjectType::NoInput,
+                        _ => return Err(IrodsError::Other("Invalid value for ObjectType".into())),
+                    });
                     State::Mode
                 }
+                (State::Mode, Event::Start(e)) if e.name().as_ref() == b"dataMode" => {
+                    State::ModeInner
+                }
+                (State::ModeInner, Event::Text(e)) => {
+                    mode = Some(e.unescape()?.parse()?);
+                    State::Id
+                }
+                (State::Id, Event::Start(e)) if e.name().as_ref() == b"dataId" => State::IdInner,
+                (State::IdInner, Event::Text(e)) => {
+                    id = Some(e.unescape()?.parse()?);
+                    State::Checksum
+                }
+                (State::Checksum, Event::Empty(e)) if e.name().as_ref() == b"chksum" => {
+                    checksum = None;
+                    State::OwnerName
+                }
+                (State::Checksum, Event::Start(e)) if e.name().as_ref() == b"chksum" => {
+                    State::ChecksumInner
+                }
+                (State::ChecksumInner, Event::Text(e)) => {
+                    checksum = Some(e.unescape()?.parse()?);
+                    State::OwnerName
+                }
+                (State::OwnerName, Event::Start(e)) if e.name().as_ref() == b"ownerName" => {
+                    State::OwnerNameInner
+                }
+                (State::OwnerNameInner, Event::Text(e)) => {
+                    owner_name = Some(e.unescape()?.to_string());
+                    State::OwnerZone
+                }
+                (State::OwnerZone, Event::Start(e)) if e.name().as_ref() == b"ownerZone" => {
+                    State::OwnerZoneInner
+                }
+                (State::OwnerZoneInner, Event::Text(e)) => {
+                    owner_zone = Some(e.unescape()?.to_string());
+                    State::CreateTime
+                }
+                (State::CreateTime, Event::Start(e)) if e.name().as_ref() == b"createTime" => {
+                    State::CreateTimeInner
+                }
+                (State::CreateTimeInner, Event::Text(e)) => {
+                    create_time = Some(e.unescape()?.parse()?);
+                    State::ModifyTime
+                }
+                (State::ModifyTime, Event::Start(e)) if e.name().as_ref() == b"modifyTime" => {
+                    State::ModifyTimeInner
+                }
+                (State::ModifyTimeInner, Event::Text(e)) => {
+                    modify_time = Some(e.unescape()?.parse()?);
+                    return Ok(RodsObjStat {
+                        size: size.ok_or(IrodsError::Other("Missing size".into()))?,
+                        object_type: object_type
+                            .ok_or(IrodsError::Other("Missing object type".into()))?,
+                        mode: mode.ok_or(IrodsError::Other("Missing mode".into()))?,
+                        id: id.ok_or(IrodsError::Other("Missing id".into()))?,
+                        checksum,
+                        owner_name: owner_name
+                            .ok_or(IrodsError::Other("Missing owner name".into()))?,
+                        owner_zone: owner_zone
+                            .ok_or(IrodsError::Other("Missing owner zone".into()))?,
+                        create_time: create_time
+                            .ok_or(IrodsError::Other("Missing create time".into()))?,
+                        modify_time: modify_time
+                            .ok_or(IrodsError::Other("Missing modify time".into()))?,
+                    });
+                }
+                (_, Event::Eof) => {
+                    return Err(IrodsError::Other("Unexpected EOF".into()));
+                }
+                state => state.0,
             }
         }
-
     }
 }
